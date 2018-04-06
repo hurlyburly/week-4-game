3b9b20a8e0a8e2dff9ed502c2b9820ad1d028e72
Kalsang Bhutia <35473365+hurlyburly@users.noreply.github.com>
2018-04-06 17:41:48 -0500

    added more game logic in javascript and style the page a bit. Added a favicon as well

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,6 @@
-// 2. The player will have to guess the answer, just like in Hangman. This time, though, the player will guess with numbers instead of letters. 
-// 3. Here's how the app works:
-// * There will be four crystals displayed as buttons on the page.
+//To Do:
 // * The player will be shown a random number at the start of the game.
-// * When the player clicks on a crystal, it will add a specific amount of points to the player's total score. 
+// * When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 //   * Your game will hide this amount until the player clicks a crystal.
 //   * When they do click one, update the player's score counter.
 // * The player wins if their total score matches the random number from the beginning of the game.
@@ -11,24 +9,76 @@
 //   * When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
 // * The app should show the number of games the player wins and loses. To that end, do not refresh the page as a means to restart the game.
 // * The random number shown at the start of the game should be between 19 - 120.
-// * Each crystal should have a random hidden value between 1 - 12.
+// *  Each crystal should have a random hidden value between 1 - 12.
+
 //global variables
-var wizard=0;
-var playerNumber=0;
-var crystalNumbers = [];
+var wizardScore = [Math.floor(Math.random() * (120 - 19) + 19)];
+var playerScore = 0;
+var blueCrystal = Math.floor(Math.random() * 12 + 1);
+var orangeCrystal = Math.floor(Math.random() * 12 + 1);
+var greenCrystal = Math.floor(Math.random() * 12 + 1);
+var redCrystal = Math.floor(Math.random() * 12 + 1);
+var wins;
+var losses;
+var game = false;
 
-function randomNumber(min,max){
-var wizardNumber=wizard[Math.round(Math.random() *(max-min)+min)];
-
-$(".wizard").html="The wizard:"+wizardNumber;
+//List of functions to call
+//update player score
+function collectCrystals() {
+  $(".blue-crystal").on("click", function() {
+    playerScore = playerScore + blueCrystal;
+  });
+  $(".orange-crystal").on("click", function() {
+    playerScore = playerScore + orangeCrystal;
+  });
+  $(".green-crystal").on("click", function() {
+    playerScore = playerScore + greenCrystal;
+  });
+  $(".red-crystal").on("click", function() {
+    playerScore = playerScore + redCrystal;
+  });
 }
-randomNumber(19,120);
-function winGame(){
-
+//start game code
+function startGame() {
+  $(".start-game").on("click", function() {
+    game = true;
+  });
+  $(".wizard").html = "Wizard: " + wizardNumber;
 }
-function loseGame(){
+
+//winning game code
+function winGame() {
+  wins++;
+  $("wins").append("Wins: " + wins);
+  gameStart = false;
+}
+//losing game code
+function loseGame() {
+  losses++;
+  $("losses").append("Losses: " + losses);
+  gameStart = false;
 }
 
-$(document).on("click", function(){
+//logic of the game
 
+function gameLogic() {
+  if (playerScore !== wizardScore) {
+    if (playerScore > wizardScore) {
+      loseGame();
+    } else {
+      collectCrystals();
+    }
+  } else {
+    winGame();
+  }
+}
+
+//starting the game
+$(document).on("click", function() {
+  if (game) {
+    gameLogic();
+  } else {
+    startGame();
+    wizardScore();
+  }
 });
